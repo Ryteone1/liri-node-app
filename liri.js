482270fbd9320208cd25f828caf5e30d226a2f2c
ryteone1 <rynlacy@yahoo.com>
2017-10-05 01:38:28 -0400

    Got Spotify basic result to display

--- liri.js
+++ liri.js
@@ -7,9 +7,28 @@ var client = new Twitter(keys.twitterKeys);
 var params = {screen_name: 'RyteCoding'};
 client.get('statuses/user_timeline', params, function(error, tweets, response) {
   if (!error) {
-    console.log(tweets);
-    
+    // console.log(tweets);
+    for(var i = 0; i < tweets.length; i++) {
+    	console.log(' ');
+        console.log(tweets[i].created_at + "\n" + tweets[i].text);
+        console.log(' ');
+    }
   }
+});
+
+
+// SPOTIFY API ========================================================================
+
+var Spotify = require('node-spotify-api');
+ 
+var spotify = new Spotify(keys.spotifyKey);
+ 
+spotify.search({ type: 'track', query: 'All the Small Things' }, function(err, data) {
+  if (err) {
+    return console.log('Error occurred: ' + err);
+  }
+ 
+console.log(data); 
 });
 
 
